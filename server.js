5288d2d5d55b8821ab8170d061c08630ee126299
Matt Meleen <mmeleen@utexas.edu>
2020-07-17 19:45:44 -0500

    Created server file with setup and routes

---
+++ server.js
@@ -0,0 +1,18 @@
+// Dependencies
+var express = require("express");
+
+// Express Setup
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Start server - Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
