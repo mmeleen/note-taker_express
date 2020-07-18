f4b0c95faf917d3a9f12c3e6e7b76bae7dc8e27c
Matt Meleen <mmeleen@utexas.edu>
2020-07-17 22:16:08 -0500

    Added dependencies, console.logs

--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -1,21 +1,22 @@
-// Load Data
-var noteData = require("../../db/db.json");
+//Dependencies
+var fs = require("fs");
+var path = require("path");
 
 // Routes
 module.exports = function(app) {
   // API GET Request
   app.get("/api/notes", function(req, res) {
-    //
+    console.log("get");
   });
 
   // API POST Request
   app.post("/api/notes", function(req, res) {
-    //
+    console.log("post");
   });
 
   // API DELETE
   app.delete("/api/notes/:id", function(req, res) {
-    //
+    console.log("delete");
   });
 };
 
