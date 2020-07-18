6d603b06d8655f8de10a43f31942265a2cf248df
Matt Meleen <mmeleen@utexas.edu>
2020-07-17 19:59:44 -0500

    Basic routes

--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -2,5 +2,20 @@
 var noteData = require("../../db/db.json");
 
 // Routes
+module.exports = function(app) {
+  // API GET Request
+  app.get("/api/notes", function(req, res) {
+    //
+  });
 
+  // API POST Request
+  app.post("/api/notes", function(req, res) {
+    //
+  });
+
+  // API DELETE
+  app.delete("/api/notes/:id", function(req, res) {
+    //
+  });
+};
 
