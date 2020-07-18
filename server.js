1571fd19bd97c3210c56a24d0397bf86fd31d460
Matt Meleen <mmeleen@utexas.edu>
2020-07-17 22:14:56 -0500

    Fixed routes, added express.static('public')

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./public/routes/apiRoutes")(app);
+require("./public/routes/htmlRoutes")(app);
 
 // Start server - Listener
 app.listen(PORT, function() {
